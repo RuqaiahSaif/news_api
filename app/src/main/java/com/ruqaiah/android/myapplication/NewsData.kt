770cd184c2bbc7cd3076a960b52dde733d58fd46
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2020-12-21 20:43:00 +0300

    second commit

--- app/src/main/java/com/ruqaiah/android/myapplication/NewsData.kt
+++ app/src/main/java/com/ruqaiah/android/myapplication/NewsData.kt
@@ -4,9 +4,14 @@ import java.util.*
 
 data class NewsData (
     var id:Int,
+      @SerializedName("news_title")
     var title:String,
+     @SerializedName("news_details")
     var details:String,
+    @SerializedName("news_date")
     var date: Date,
+     @SerializedName("news_image")
     var image:String,
+       @SerializedName("id_cat")
     var cat_id:Int){
 }
